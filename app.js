40a6ca19f7d8091c3a972522053023d28ac3716b
Harish Tagadghar <harishtgtg@gmail.com>
2020-05-13 00:34:01 +0530

    about to find index of the elemetn

--- app.js
+++ app.js
@@ -14,18 +14,27 @@ fs.createReadStream(__dirname + '/A.csv')
    for (let i = 0; i < results.length; i++) {
         let array = Object.values(results[i])
        let array1 = array.map(el => el.split(" ")[0])
-        value.push(array1) > -1
+        value.push(array1) 
      
       }
 let index = []
+for (let i = 0; i < value.length; i++) {
+    index.push(value[i].toString())
+}
+for (let i = 0; i < index.length; i++) {
+for (let j = 0; j < words.length; j++) {
+   if(words[j] == index[i]){
+       console.log(index[i]);
+       
+   }   
 
-index.push(value.toString())
+}
+}
 
 
 
-
-console.log(results.length);
-console.log(index);
+// console.log(results.length);
+// console.log(index);
 
 
   });
